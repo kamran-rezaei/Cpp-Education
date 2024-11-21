8c3821e82a2e701b9e4a6f256e5a73cb35c667f2
Kamran Rezaei <36191813+kamran5775@users.noreply.github.com>
2024-11-22 01:21:05 +0330

    Add hello_world.cpp to Basics

---
+++ hello_world.cpp
@@ -0,0 +1,9 @@
+#include <iostream> // Include the library for input/output
+using namespace std;
+
+int main() {
+    // Print a simple message
+    cout << "Hello, World!" << endl;
+
+    return 0; // Indicate that the program ran successfully
+}
